d049ce680f651b6894e040fca28a0213cb8f78b4
Kirill Aleksandrov <happy.nan@mail.ru>
2020-08-07 15:19:43 +0700

    delete space

--- client/src/components/Content/ComponentsOfContent/AddUser/AddUser.jsx
+++ client/src/components/Content/ComponentsOfContent/AddUser/AddUser.jsx
@@ -2,9 +2,9 @@ import React, { useState } from 'react'
 
 const AddUser = ({add})=>{
   const [user, setUser] = useState({
-    name:" ",
-    tnum:" ",
-    uri:" "
+    name:"",
+    tnum:"",
+    uri:""
   })
   const submitHandler = (e)=>{
     e.preventDefault()
@@ -18,6 +18,7 @@ const AddUser = ({add})=>{
     setUser({...user, [e.target.name]:e.target.value})
 
   }
+  
   return(
     <div className="row">
       <form className="col s12" onSubmit={submitHandler}>
@@ -25,17 +26,14 @@ const AddUser = ({add})=>{
       
         <div className="row">
           <div className="input-field col s6">
-            <p>
               <input  id="name" type="text" name="name" className="validate" onChange={handleChange} value={user.name} className="active" placeholder="Name" />
               <label htmlFor="name">Name</label>
-            </p>
           </div>
+
           <div className="input-field col s6">
-            <p>
 
               <input id="tnum" type="text" name="tnum" className="validate"  onChange={handleChange} value={user.tnum}/> 
               <label htmlFor="tnum">Number</label>
-            </p>
 
           </div>
 
@@ -43,11 +41,9 @@ const AddUser = ({add})=>{
         <div className="row">
 
           <div className="input-field col s12">
-            <p>
 
-              <input id="uri" type="text" name="uri" className="validate" onChange={handleChange} value={user.uri}/>
+              <input id="uri" type="text" name="uri"  onChange={handleChange} value={user.uri}/>
               <label htmlFor="uri">URI of image</label>
-            </p>
 
           </div>
 
